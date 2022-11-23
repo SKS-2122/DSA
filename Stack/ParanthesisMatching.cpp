2014687db0344c4fa8b0255fcff7ebbe91bb7189
SKS-2122 <sa02122000@gmail.com>
2022-11-23 09:21:10 +0530

    Initial Commit

--- Stack/ParanthesisMatching.cpp
+++ Stack/ParanthesisMatching.cpp
@@ -1,127 +1,113 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include<strings.h>
-struct Node
-{
- char data;
- struct Node *next;
-}*top=NULL;
-void push(char x)
-{
- struct Node *t;
- t=(struct Node*)malloc(sizeof(struct Node));
- 
- if(t==NULL)
- printf("stack is full\n");
- else
- {
- t->data=x;
- t->next=top;
- top=t;
- }
- 
-}
-char pop()
-{
- struct Node *t;
- char x=-1;
- 
- if(top==NULL)
- printf("Stack is Empty\n");
- else
- {
- t=top;
- top=top->next;
- x=t->data;
- free(t);
- }
- return x;
-}
-void Display()
-{
- struct Node *p;
- p=top;
- while(p!=NULL)
- {
- printf("%d ",p->data);
- p=p->next;
- }
- printf("\n");
-}
-int isBalanced(char *exp)
-{
- int i;
- 
- for(i=0;exp[i]!='\0';i++)
- {
- if(exp[i]=='(')
- push(exp[i]);
- else if(exp[i]==')')
- {
- if(top==NULL)
- return 0;
- pop();
- }
- }
- if(top==NULL)
- return 1;
- else
- return 0;
-}
-int pre(char x)
-{
- if(x=='+' || x=='-')
- return 1;
- else if(x=='*' || x=='/')
- return 2;
- return 0;
-}
- 
-int isOperand(char x)
-{
- if(x=='+' || x=='-' || x=='*' || x=='/')
- return 0;
- else
- return 1;
- 
-}
- 
-char * InToPost(char *infix)
-{
- int i=0,j=0;
- char *postfix;
- int len=strlen(infix);
- postfix=(char *)malloc((len+2)*sizeof(char));
- 
- while(infix[i]!='\0')
- {
- if(isOperand(infix[i]))
- postfix[j++]=infix[i++];
- else
- {
- if(pre(infix[i])>pre(top->data))
- push(infix[i++]);
- else
- {
- postfix[j++]=pop();
- }
- }
- }
- while(top!=NULL)
- postfix[j++]=pop();
- postfix[j]='\0';
- return postfix;
-}
- 
+#include <iostream>
+#include <stack>
+using namespace std;
+// defines the Boolean function for operator, operand, equalOrhigher precedence and the string conversion function.
+bool IsOperator(char);
+bool IsOperand(char);
+bool eqlOrhigher(char, char);
+string convert(string);
+
 int main()
 {
- char *infix="a+b*c-d/e";
- push('#');
- 
- char *postfix=InToPost(infix);
- 
- printf("%s ",postfix);
- 
- 
- return 0;
+    string infix_expression, postfix_expression;
+    int ch;
+    do
+    {
+        cout << " Enter an infix expression: ";
+        cin >> infix_expression;
+        postfix_expression = convert(infix_expression);
+        cout << "\n Your Infix expression is: " << infix_expression;
+        cout << "\n Postfix expression is: " << postfix_expression;
+        cout << "\n \t Do you want to enter infix expression (1/ 0)?";
+        cin >> ch;
+        // cin.ignore();
+    } while (ch == 1);
+    return 0;
 }
+
+// define the IsOperator() function to validate whether any symbol is operator.
+/* If the symbol is operator, it returns true, otherwise false. */
+bool IsOperator(char c)
+{
+    if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^')
+        return true;
+    return false;
+}
+
+// IsOperand() function is used to validate whether the character is operand.
+bool IsOperand(char c)
+{
+    if (c >= 'A' && c <= 'Z') /* Define the character in between A to Z. If not, it returns False.*/
+        return true;
+    if (c >= 'a' && c <= 'z') // Define the character in between a to z. If not, it returns False. */
+        return true;
+    if (c >= '0' && c <= '9') // Define the character in between 0 to 9. If not, it returns False. */
+        return true;
+    return false;
+}
+// here, precedence() function is used to define the precedence to the operator.
+int precedence(char op)
+{
+    if (op == '+' || op == '-') /* it defines the lowest precedence */
+        return 1;
+    if (op == '*' || op == '/')
+        return 2;
+    if (op == '^') /* exponent operator has the highest precedence */
+return 3;
+return 0;
+}
+/* The eqlOrhigher() function is used to check the higher or equal precedence of the two operators in infix expression. */
+        bool eqlOrhigher(char op1, char op2)
+        {
+            int p1 = precedence(op1);
+            int p2 = precedence(op2);
+            if (p1 == p2)
+            {
+                if (op1 == '^')
+                    return false;
+                return true;
+            }
+            return (p1 > p2 ? true : false);
+        }
+
+    /* string convert() function is used to convert the infix expression to the postfix expression of the Stack */
+    string convert(string infix)
+    {
+        stack<char> S;
+        string postfix = "";
+        char ch;
+
+        S.push('(');
+        infix += ')';
+
+        for (int i = 0; i < infix.length(); i++)
+        {
+            ch = infix[i];
+
+            if (ch == ' ')
+                continue;
+            else if (ch == '(')
+                S.push(ch);
+            else if (IsOperand(ch))
+                postfix += ch;
+            else if (IsOperator(ch))
+            {
+                while (!S.empty() && eqlOrhigher(S.top(), ch))
+                {
+                    postfix += S.top();
+                    S.pop();
+                }
+                S.push(ch);
+            }
+            else if (ch == ')')
+            {
+                while (!S.empty() && S.top() != '(')
+                {
+                    postfix += S.top();
+                    S.pop();
+                }
+                S.pop();
+            }
+        }
+        return postfix;
+    }
